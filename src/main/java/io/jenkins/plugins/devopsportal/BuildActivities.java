9ef3e6190f35af29db05da9ad05f4448f493a2b5
Aziz JOUBI <remi.bello.pro@gmail.com>
2022-11-11 19:05:02 +0100

    WIP Limitation des activités de build

--- src/main/java/io/jenkins/plugins/devopsportal/BuildActivities.java
+++ src/main/java/io/jenkins/plugins/devopsportal/BuildActivities.java
@@ -7,15 +7,15 @@ package io.jenkins.plugins.devopsportal;
  */
 public enum BuildActivities {
 
-    INTEGRATION("Features integration"),
+    //INTEGRATION("Features integration"),
     BUILD("Build"),
     UT("Unit Tests"),
     QA("Quality Audit"),
     SECU("Security Audit"),
     DEPLOY("Deployment"),
-    PERF("Performance Tests"),
-    QUALIF("Qualification Tests"),
-    UAT("User Acceptance Tests");
+    PERF("Performance Tests");
+    //QUALIF("Qualification Tests"),
+    //UAT("User Acceptance Tests");
 
     private final String label;
 
