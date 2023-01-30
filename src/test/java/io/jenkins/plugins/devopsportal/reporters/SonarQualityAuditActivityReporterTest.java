eea11505359c2a23a557c4a781d6a169b91c435c
Aziz JOUBI <remi.bello.pro@gmail.com>
2023-01-30 11:28:24 +0100

    WIP UT for SonarQualityAuditActivityReporter

---
+++ src/test/java/io/jenkins/plugins/devopsportal/reporters/SonarQualityAuditActivityReporterTest.java
@@ -0,0 +1,61 @@
+package io.jenkins.plugins.devopsportal.reporters;
+
+import hudson.model.FreeStyleProject;
+import hudson.model.Label;
+import hudson.model.Result;
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+public class SonarQualityAuditActivityReporterTest {
+
+    @Rule
+    public JenkinsRule jenkins = new JenkinsRule();
+
+    final String applicationName = "My Application";
+    final String applicationVersion = "1.0.3";
+    final String applicationComponent = "backend";
+
+    @Test
+    public void testConfigRoundtrip() throws Exception {
+        FreeStyleProject project = jenkins.createFreeStyleProject();
+        SonarQualityAuditActivityReporter reporter = new SonarQualityAuditActivityReporter(
+                applicationName, applicationVersion, applicationComponent);
+        reporter.setProjectKey("project-key");
+        reporter.setAcceptInvalidCertificate(true);
+        project.getBuildersList().add(reporter);
+        project = jenkins.configRoundtrip(project);
+        jenkins.assertEqualDataBoundBeans(
+                reporter,
+                project.getBuildersList().get(0)
+        );
+    }
+
+    @Test
+    public void testScriptedPipeline() throws Exception {
+        String agentLabel = "my-agent";
+        jenkins.createOnlineSlave(Label.get(agentLabel));
+        WorkflowJob job = jenkins.createProject(WorkflowJob.class, "test-scripted-pipeline");
+        String pipelineScript = "node {\n" +
+                "  reportSonarQubeAudit(\n" +
+                "       applicationName: '" + applicationName + "',\n" +
+                "       applicationVersion: '" + applicationVersion + "',\n" +
+                "       applicationComponent: '" + applicationComponent + "',\n" +
+                "       projectKey: 'project-key',\n" +
+                "       qualityGatePassed: false\n" +
+                "  )\n" +
+                "}";
+        job.setDefinition(new CpsFlowDefinition(pipelineScript, true));
+        WorkflowRun completedBuild = jenkins.assertBuildStatus(Result.SUCCESS, job.scheduleBuild2(0));
+        jenkins.assertLogContains(
+                "Report build activity 'QUALITY_AUDIT' for application '" + applicationName + "' version "
+                        + applicationVersion + " component '" + applicationComponent + "'",
+                completedBuild
+        );
+
+    }
+
+}
