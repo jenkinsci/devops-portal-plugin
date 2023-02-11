b6f25496ab47949c7a0da5adfd07b2ad58c54905
rbello <remi@evolya.fr>
2023-02-11 22:23:22 +0100

    WIP Remove sonar library

--- src/main/java/io/jenkins/plugins/devopsportal/utils/SonarApiClient.java
+++ src/main/java/io/jenkins/plugins/devopsportal/utils/SonarApiClient.java
@@ -1,10 +1,8 @@
 package io.jenkins.plugins.devopsportal.utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.StatusLine;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -15,9 +13,8 @@ import org.apache.http.ssl.SSLContextBuilder;
 import javax.net.ssl.SSLContext;
 import java.net.URISyntaxException;
 import java.security.GeneralSecurityException;
-import java.util.Base64;
-import java.util.Map;
-import java.util.function.BiConsumer;
+import java.util.*;
+import java.util.function.Consumer;
 
 public class SonarApiClient {
 
@@ -41,33 +38,75 @@ public class SonarApiClient {
             }
         }
     }
-    public void getMetrics(String projectKey, String... metricKeys) {
+
+    public List<Map<String, Object>> getIssues(String projectKey) {
+        return execute("/api/issues/search", "issues", request -> {
+            request.setParameter("componentKeys", projectKey);
+            request.setParameter("ps", "500");
+            request.setParameter("resolved", "no");
+            request.setParameter("severities", "MAJOR,CRITICAL,BLOCKER");
+            request.setParameter("statuses", "OPEN");
+            request.setParameter("types", "BUG,VULNERABILITY,CODE_SMELL");
+        });
+    }
+
+    public List<Map<String, Object>> getMetrics(String projectKey) {
+        return getMetrics(
+                projectKey,
+                // Quality Gate
+        "alert_status",
+                "quality_gate_details",
+                // Scores
+                "sqale_rating", // Maintainability (code smells)
+                "reliability_rating", // Reliability (bugs)
+                "security_rating", // Security (vulnerabilities)
+                "security_review_rating", // Security review (hotspots)
+                // Metrics
+                "coverage", // Test coverage
+                "duplicated_lines_density", // Duplication
+                "ncloc" // Lines of code
+        );
+    }
+
+    public List<Map<String, Object>> getMetrics(String projectKey, String... metricKeys) {
+        return execute("/api/measures/search", "measures", request -> {
+            request.setParameter("projectKeys", projectKey);
+            request.setParameter("metricKeys", String.join(",", metricKeys));
+        });
+    }
+
+    public List<Map<String, Object>> getHotspots(String projectKey) {
+        return execute("/api/hotspots/search", "hotspots", request -> {
+            request.setParameter("projectKey", projectKey);
+            request.setParameter("status", "TO_REVIEW");
+            request.setParameter("ps", "500");
+        });
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Map<String, Object>> execute(String path, String listAttribute, Consumer<URIBuilder> consumer) {
+        HttpGet request;
         try {
-            URIBuilder builder = new URIBuilder(url + "/api/measures/search");
-            builder.setParameter("projectKeys", projectKey);
-            builder.setParameter("metricKeys", String.join(",", metricKeys));
-            execute(new HttpGet(builder.build()), (status, response) -> {
-                System.out.println("Status:" + status);
-                System.out.println("Response: " + response);
-            });
+            URIBuilder builder = new URIBuilder(url + path);
+            consumer.accept(builder);
+            request = new HttpGet(builder.build());
         }
         catch (URISyntaxException ex) {
             throw new RuntimeException(ex);
         }
-    }
-
-    @SuppressWarnings("unchecked")
-    public void execute(HttpUriRequest request, BiConsumer<StatusLine, Map<String, Object>> consumer) {
         request.setHeader("Authorization", "Basic " + authenticationToken);
-        try (CloseableHttpClient httpClient = builder.build()) {
+        try (CloseableHttpClient httpClient = this.builder.build()) {
             CloseableHttpResponse response = httpClient.execute(request);
             if (response.getStatusLine().getStatusCode() != 200) {
-                consumer.accept(response.getStatusLine(), null);
+                return null;
             }
             else {
                 ObjectMapper mapper = new ObjectMapper();
-                Map<String, Object> value = mapper.readValue(response.getEntity().getContent(), Map.class);
-                consumer.accept(response.getStatusLine(), value);
+                Map<?, ?> map = mapper.readValue(response.getEntity().getContent(), Map.class);
+                if (map.containsKey(listAttribute)) {
+                    return (List<Map<String, Object>>) map.get(listAttribute);
+                }
+                return Collections.emptyList();
             }
         }
         catch (Exception ex) {
@@ -81,8 +120,13 @@ public class SonarApiClient {
                 "squ_f9206dc2876419bd70a3cbe0a64d5e3b09de9938",
                 true
         );
-        client.getMetrics("io.jenkins.plugins.devops-portal:1.0", "alert_status", "quality_gate_details");
+        debug("metrics", client.getMetrics("io.jenkins.plugins.devops-portal:1.0"));
+        debug("issues", client.getIssues("io.jenkins.plugins.devops-portal:1.0"));
+        debug("hotspots", client.getHotspots("io.jenkins.plugins.devops-portal:1.0"));
+    }
 
+    private static void debug(String data, List<Map<String, Object>> values) {
+        System.out.println(" " + data +" >>> " + values.size());
     }
 
 }
